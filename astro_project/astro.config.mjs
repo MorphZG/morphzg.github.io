35f16a0a09ec07fb519058c8a9855a768b360cf6
MorphZG <zoran1607@gmail.com>
2025-01-15 13:05:20 +0100

    fix(deployment): minor config corrections

--- astro_project/astro.config.mjs
+++ astro_project/astro.config.mjs
@@ -5,5 +5,10 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
     server: { port: 3000 },
     site: "https://morphzg.github.io",
+    output: "static",
+    outDir: "./dist",
+    build: {
+        assets: "astro"
+    }
     
 });
